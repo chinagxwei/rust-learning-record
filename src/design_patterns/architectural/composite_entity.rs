86795b9180582d1819d0baf9813e30dd726b72cd
kumatata <chinagxwei@gmail.com>
2021-02-02 14:47:23 +0800

    change project

--- src/design_patterns/architectural/composite_entity.rs
+++ src/design_patterns/architectural/composite_entity.rs
@@ -1,3 +1,12 @@
+//!
+//! 组合实体模式（Composite Entity Pattern）。
+//!
+//! 组合实体（Composite Entity） - 它可以是粗粒的，或者可以包含一个粗粒度对象，用于持续生命周期。
+//! 粗粒度对象（Coarse-Grained Object） - 该对象包含依赖对象。它有自己的生命周期，也能管理依赖对象的生命周期。
+//! 依赖对象（Dependent Object） - 依赖对象是一个持续生命周期依赖于粗粒度对象的对象。
+//! 策略（Strategies） - 策略表示如何实现组合实体。
+//!
+
 struct DependentObject1 {
     data: Option<&'static str>
 }
