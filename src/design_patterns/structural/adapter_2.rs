918bed4ea989c4b9fc498d7179abcf7a05b67486
kumatata <chinagxwei@gmail.com>
2021-02-02 16:33:41 +0800

    change project

--- src/design_patterns/structural/adapter_2.rs
+++ src/design_patterns/structural/adapter_2.rs
@@ -1,3 +1,22 @@
+//!
+//! 适配器模式（Adapter Pattern）是作为两个不兼容的接口之间的桥梁。
+//! 这种类型的设计模式属于结构型模式，它结合了两个独立接口的功能。
+//! 这种模式涉及到一个单一的类，该类负责加入独立的或不兼容的接口功能。
+//!
+//! 我们有一个 MediaPlayer 接口和一个实现了 MediaPlayer 接口的实体类 AudioPlayer。
+//! 默认情况下，AudioPlayer 可以播放 mp3 格式的音频文件。
+//!
+//! 我们还有另一个接口 AdvancedMediaPlayer 和实现了 AdvancedMediaPlayer 接口的实体类。
+//! 该类可以播放 vlc 和 mp4 格式的文件。
+//!
+//! 我们想要让 AudioPlayer 播放其他格式的音频文件。
+//! 为了实现这个功能，我们需要创建一个实现了 MediaPlayer 接口的适配器类 MediaAdapter，并使用 AdvancedMediaPlayer 对象来播放所需的格式。
+//!
+//! AudioPlayer 使用适配器类 MediaAdapter 传递所需的音频类型，不需要知道能播放所需格式音频的实际类。
+//! AdapterPatternDemo 类使用 AudioPlayer 类来播放各种格式。
+//!
+
+
 use std::fmt::{Display, Formatter};
 
 enum AudioType {
--- src/design_patterns/structural/flyweight_2.rs
+++ src/design_patterns/structural/flyweight_2.rs
@@ -1,3 +1,8 @@
+//!
+//! 享元模式（Flyweight Pattern）主要用于减少创建对象的数量，以减少内存占用和提高性能。
+//! 这种类型的设计模式属于结构型模式，它提供了减少对象数量从而改善应用所需的对象结构的方式。
+//!
+
 use std::collections::HashMap;
 use std::cell::RefCell;
 use std::rc::Rc;
